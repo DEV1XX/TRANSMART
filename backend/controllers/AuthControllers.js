ab3990d8393090a9996d4899fbeda0ad495340d0
Aditya Narayan Mali <sabitrimali76@gmail.com>
2025-01-17 17:34:47 +0530

    coded auth controllers

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -1,1 +1,71 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+//SIGNUP ROUTE
+const signup = async (req,res) =>{
+    const {email, password, name} = req.body;
+
+    if (!email || !password || !name) {
+        return res.status(400).json({ success: false, message: "All fields are required!" });
+    }
+
+    try{
+        let user = await User.findOne({ email });
+        if(user)  return res.status(400).json({success: false, message: "Email already in use!"});
+        let hashedpw = await bcrypt.hash(password,10); // 10 is the number of salt rounds
+        
+        user = await User.create({
+            email,
+            password: hashedpw,
+            name
+        });
+        
+        return res.status(201).json({ success: true, message: "Signup successful!" });
+
+    }catch(error){
+        return res.status(500).json({success: false, message: "An error occurred during signup." });
+    }
+}
+//LOGIN ROUTE 
+const login = async (req,res) =>{
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({ success: false, message: "All fields are required!" });
+    }
+    try {
+        let user = await User.findOne({ email });
+        if(!user){
+            return res.status(400).json({success: false, message: "Invalid email or password!"});
+        }
+        //compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({success: false, message: "Invalid email or password!"});
+        }
+        //GENERATE AND SET COOKIES
+        const token = jwt.sign(
+            {userId: user._id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '24h'}
+        )
+        return res.status(200).json({ success: true, token, message: "Login successful!" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success: false, message:"Login Unsuccessful!"})
+    }
+}
+
+//LOGOUT ROUTE
+const logout = async (req,res)=>{
+    try {
+        res.clearCookie('token');
+        return res.status(200).json({success: true, message: 'Logout successful !'});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success:false, message:"Logout failed!"})
+    }
+}
+
+module.exports = {signup,login,logout}
